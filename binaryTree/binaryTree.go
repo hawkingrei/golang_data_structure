02c302cd76c772914c7f22b465afeb82b8e99e7f
真人 <weizhen.wang@duitang.com>
2017-02-12 21:38:31 +0800

    update

--- binaryTree/binaryTree.go
+++ binaryTree/binaryTree.go
@@ -1,187 +1,116 @@
 package binaryTree
 
 import (
-	"math"
+	"container/list"
 )
 
-//二叉树节点
-type BinTreeNode struct {
-	data   interface{}  //数据域
-	parent *BinTreeNode //父节点
-	lChild *BinTreeNode //左孩子
-	rChild *BinTreeNode //右孩子
-	height int          //以该结点为根的子树的高度
-	size   int          //该结点子孙数(包括结点本身)
+//二叉树
+type binaryTree struct {
+	root   *BinTreeNode //根节点
+	height int
+	size   int
 }
 
-func NewBinTreeNode(e interface{}) *BinTreeNode {
-	return &BinTreeNode{data: e, size: 1}
+func NewBinaryTree(root *BinTreeNode) *binaryTree {
+	return &binaryTree{root: root}
 }
 
-//获得节点数据
-func (this *BinTreeNode) GetData() interface{} {
-	if this == nil {
+//获得二叉树总结点数
+func (this *binaryTree) GetSize() int {
+	return this.root.size
+}
+
+//判断二叉树是否为空
+func (this *binaryTree) IsEmpty() bool {
+	return this.root != nil
+}
+
+//获得二叉树根节点
+func (this *binaryTree) GetRoot() *BinTreeNode {
+	return this.root
+}
+
+//获得二叉树高度，根节点层为0
+func (this *binaryTree) GetHeight() int {
+	return this.root.height
+}
+
+//获得第一个与数据e相等的节点
+func (this *binaryTree) Find(e interface{}) *BinTreeNode {
+	if this.root == nil {
 		return nil
 	}
-	return this.data
+	p := this.root
+	return isEqual(e, p)
 }
 
-//设置节点数据
-func (this *BinTreeNode) SetData(e interface{}) {
-	this.data = e
-}
-
-//判断是否有父亲
-func (this *BinTreeNode) HasParent() bool {
-	return this.parent != nil
-}
-
-//获得父亲节点
-func (this *BinTreeNode) GetParent() *BinTreeNode {
-	if !this.HasParent() {
-		return nil
+func isEqual(e interface{}, node *BinTreeNode) *BinTreeNode {
+	if e == node.GetData() {
+		return node
 	}
-	return this.parent
+
+	if node.HasLChild() {
+		lp := isEqual(e, node.GetLChild())
+		if lp != nil {
+			return lp
+		}
+	}
+
+	if node.HasRChild() {
+		rp := isEqual(e, node.GetRChild())
+		if rp != nil {
+			return rp
+		}
+
+	}
+
+	return nil
 }
 
-//设置父亲节点
-func (this *BinTreeNode) setParent(p *BinTreeNode) {
-	this.parent = p
-	// this.parent.SetHeight() //更新父结点及其祖先高度
-	// this.parent.SetSize()   //更新父结点及其祖先规模
+//先序遍历，并保存在链表里
+func (this *binaryTree) PreOrder() *list.List {
+	traversal := list.New()
+	preOrder(this.root, traversal)
+	return traversal
 }
 
-//断开与父亲的关系
-func (this *BinTreeNode) CutOffParent() {
-	if !this.HasParent() {
+func preOrder(rt *BinTreeNode, l *list.List) {
+	if rt == nil {
 		return
 	}
-	if this.IsLChild() {
-		this.parent.lChild = nil //断开该节点与父节点的连接
-	} else {
-		this.parent.rChild = nil //断开该节点与父节点的连接
-	}
-
-	this.parent = nil       //断开该节点与父节点的连接
-	this.parent.SetHeight() //更新父结点及其祖先高度
-	this.parent.SetSize()   //更新父结点及其祖先规模
+	l.PushBack(rt)
+	preOrder(rt.GetLChild(), l)
+	preOrder(rt.GetRChild(), l)
 }
 
-//判断是否有左孩子
-func (this *BinTreeNode) HasLChild() bool {
-	return this.lChild != nil
+//中序遍历，并保存在链表里
+func (this *binaryTree) InOrder() *list.List {
+	traversal := list.New()
+	inOrder(this.root, traversal)
+	return traversal
 }
 
-//获得左孩子节点
-func (this *BinTreeNode) GetLChild() *BinTreeNode {
-	if !this.HasLChild() {
-		return nil
-	}
-	return this.lChild
-}
-
-//设置当前结点的左孩子,返回原左孩子
-func (this *BinTreeNode) SetLChild(lc *BinTreeNode) *BinTreeNode {
-	oldLC := this.lChild
-	if this.HasLChild() {
-		this.lChild.CutOffParent() //断开当前左孩子与结点的关系
-	}
-	if lc != nil {
-		lc.CutOffParent() //断开lc与其父结点的关系
-		this.lChild = lc  //确定父子关系
-		lc.setParent(this)
-		this.SetHeight() //更新当前结点及其祖先高度
-		this.SetSize()   //更新当前结点及其祖先规模
-	}
-	return oldLC
-}
-
-//判断是否有右孩子
-func (this *BinTreeNode) HasRChild() bool {
-	return this.rChild != nil
-}
-
-//获得右孩子节点
-func (this *BinTreeNode) GetRChild() *BinTreeNode {
-	if !this.HasRChild() {
-		return nil
-	}
-	return this.rChild
-}
-
-//设置当前结点的右孩子,返回原右孩子
-func (this *BinTreeNode) SetRChild(rc *BinTreeNode) *BinTreeNode {
-	oldRC := this.rChild
-	if this.HasRChild() {
-		this.rChild.CutOffParent() //断开当前左孩子与结点的关系
-	}
-	if rc != nil {
-		rc.CutOffParent() //断开rc与其父结点的关系
-		this.rChild = rc  //确定父子关系
-		rc.setParent(this)
-		this.SetHeight() //更新当前结点及其祖先高度
-		this.SetSize()   //更新当前结点及其祖先规模
-	}
-	return oldRC
-}
-
-//判断是否为叶子结点
-func (this *BinTreeNode) IsLeaf() bool {
-	return !this.HasLChild() && !this.HasRChild()
-}
-
-//判断是否为某结点的左孩子
-func (this *BinTreeNode) IsLChild() bool {
-	return this.HasParent() && this == this.parent.lChild
-}
-
-//判断是否为某结点的右孩子
-func (this *BinTreeNode) IsRChild() bool {
-	return this.HasParent() && this == this.parent.rChild
-}
-
-//取结点的高度,即以该结点为根的树的高度
-func (this *BinTreeNode) GetHeight() int {
-	return this.height
-}
-
-//更新当前结点及其祖先的高度
-func (this *BinTreeNode) SetHeight() {
-	newH := 0 //新高度初始化为0,高度等于左右子树高度加1中的大者
-	if this.HasLChild() {
-		newH = int(math.Max(float64(newH), float64(1+this.GetLChild().GetHeight())))
-	}
-	if this.HasRChild() {
-		newH = int(math.Max(float64(newH), float64(1+this.GetRChild().GetHeight())))
-	}
-	if newH == this.height {
-		//高度没有发生变化则直接返回
+func inOrder(rt *BinTreeNode, l *list.List) {
+	if rt == nil {
 		return
 	}
-
-	this.height = newH //否则更新高度
-	if this.HasParent() {
-		this.GetParent().SetHeight() //递归更新祖先的高度
-	}
+	inOrder(rt.GetLChild(), l)
+	l.PushBack(rt)
+	inOrder(rt.GetRChild(), l)
 }
 
-//取以该结点为根的树的结点数
-func (this *BinTreeNode) GetSize() int {
-	return this.size
+//后序遍历，并保存在链表里
+func (this *binaryTree) PostOrder() *list.List {
+	traversal := list.New()
+	postOrder(this.root, traversal)
+	return traversal
 }
 
-//更新当前结点及其祖先的子孙数
-func (this *BinTreeNode) SetSize() {
-	this.size = 1 //初始化为1,结点本身
-	if this.HasLChild() {
-		this.size += this.GetLChild().GetSize() //加上左子树规模
+func postOrder(rt *BinTreeNode, l *list.List) {
+	if rt == nil {
+		return
 	}
-	if this.HasRChild() {
-		this.size += this.GetRChild().GetSize() //加上右子树规模
-	}
-
-	if this.HasParent() {
-		this.parent.SetSize() //递归更新祖先的规模
-	}
-
+	postOrder(rt.GetLChild(), l)
+	postOrder(rt.GetRChild(), l)
+	l.PushBack(rt)
 }
